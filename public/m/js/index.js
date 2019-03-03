5c44674cfcaa68e953e6fcb63aa38b7e02227216
liangkun97 <6765254@qq.com>
2019-03-03 11:28:27 +0800

    fourth commit file

---
+++ public/m/js/index.js
@@ -0,0 +1,13 @@
+$(function () {
+    //    滑动
+    mui('.mui-scroll-wrapper').scroll({
+        indicators: false,
+        deceleration:0.005
+    });
+
+//    轮播图
+    var gallery = mui('.mui-slider');
+    gallery.slider({
+        interval:1000
+    })
+});
